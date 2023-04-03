362fee0030da104621bc8962f1d7a138d3c82a6b
aeluro1 <103622874+aeluro1@users.noreply.github.com>
2023-04-03 15:50:26 -0400

    Final attempt at contour detection using adaptive threshold

--- pic.py
+++ pic.py
@@ -4,13 +4,20 @@ import matplotlib as plt
 import imgprocess as ip
 import time
 import os
-import sys
-
+import argparse
 outpath = "temp"
-if len(sys.argv) > 1:
-    srcpath =os.path.join("flanges", f"{sys.argv[1]}.jpg")
-else:
-    sys.exit("No image index provided")
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", type = str, required = True, help = "Image index")
+args = vars(ap.parse_args())
+##import sys
+##
+##if len(sys.argv) > 1:
+##    srcpath =os.path.join("flanges", f"{sys.argv[1]}.jpg")
+##else:
+##    sys.exit("No image index provided")
+
+srcpath = os.path.join("flanges", f"{args['image']}.jpg")
 
 def main():
     cap = cv.imread(srcpath)
@@ -23,34 +30,48 @@ def main():
 
     frame = ip.resizeImg(frame, width = 500)
 
+    original = frame
     frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    #frame = cv.GaussianBlur(frame, (11, 11), 0)
+##    frame = cv.GaussianBlur(frame, (5, 5), 0) # Review effect
     frame = cv.medianBlur(frame, 5)
 
-##    ret, frame = cv.threshold(frame, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    #(T, thresh) = cv.threshold(frame, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
     frame = cv.adaptiveThreshold(frame, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 3, 2)
+    frame = cv.bitwise_not(frame)
 
-    # Morphological open to isolate/denoise elliptical features
-##    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5,5))
-##    frame = cv.morphologyEx(frame, cv.MORPH_OPEN, kernel, iterations = 3)
+    contours, h = cv.findContours(frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    for c in contours:
+        print(len(c))
+        print(c.shape)
+        approx = cv.approxPolyDP(c, 0.03 * cv.arcLength(c, True), True)
+        if len(c) > 10 and cv.isContourConvex(approx):
+            cv.drawContours(original, [c], 0, (0, 0, 255), -1)
+            
+##    # Morphological open to remove black noise blobs
+##    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3,3))
+##    frame = cv.morphologyEx(frame, cv.MORPH_OPEN, kernel, iterations = 2)
 ##
 ##    # Find contours
-    cnts = cv.findContours(frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    frame = cv.cvtColor(frame, cv.COLOR_GRAY2BGR) # for testing to show red circles
-    for c in cnts:
-        pm = cv.arcLength(c, True)
-        approx = cv.approxPolyDP(c, 0.04 * pm, True)
-        area = cv.contourArea(c)
-        if len(approx) > 5 and area > 1000 and area < 500000:
-            ((x, y), r) = cv.minEnclosingCircle(c)
-            cv.circle(frame, (int(x), int(y)), int(r), (0, 0, 255), 2)
+##    cnts = cv.findContours(frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+##    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
+##    for c in cnts:
+##        pm = cv.arcLength(c, True)
+##        approx = cv.approxPolyDP(c, 0.04 * pm, True)
+##        area = cv.contourArea(c)
+##        if len(approx) > 5 and area > 1000 and area < 500000:
+##            ((x, y), r) = cv.minEnclosingCircle(c)
+##            cv.circle(frame, (int(x), int(y)), int(r), (0, 0, 255), 2)
+
+    frame = cv.cvtColor(frame, cv.COLOR_GRAY2BGR)
     cv.imshow("View", frame)
-
+    cv.imshow("Circled", original)
     if cv.waitKey(0) == ord('s'):
         t = time.strftime("%Y%m%d-%H%M%S")
+        frame = cv.hconcat((frame, original))
         cv.imwrite(os.path.join(outpath, f"test_{t}.png"), frame)
+
+    cv.destroyAllWindows()
         
 
 if __name__ == "__main__":
