14eccb019f4dee759eca91517ac4804df7a81b92
aeluro1 <103622874+aeluro1@users.noreply.github.com>
2023-04-03 15:50:27 -0400

    Reorganized code, added Hough circle method

--- pcd.py
+++ pcd.py
@@ -1,16 +1,16 @@
 import numpy as np
 import open3d
 import argparse
-srcpath = "flanges"
+SRCPATH = "flanges"
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--file", type = str, required = True, help = "PCD file name")
-args = vars(ap.parse_args())
-srcpath = os.path.join(srcpath, args["file"])
+
 
 def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--file", type = str, required = True, help = "PCD file name")
+    args = vars(ap.parse_args())
     
-    cloud = open3d.read_point_cloud(srcpath) # Read the point cloud
+    cloud = open3d.read_point_cloud(os.path.join(SRCPATH, args["file"])) # Read the point cloud
     open3d.draw_geometries([cloud]) # Visualize the point cloud     
 
 if __name__ == "__main__":
--- pic.py
+++ pic.py
@@ -1,82 +1,92 @@
 import cv2 as cv
 import numpy as np
-import matplotlib as plt
 import imgprocess as ip
 import time
 import os
 import argparse
-srcpath = "flanges"
-outpath = "temp"
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type = str, required = True, help = "Image index")
-args = vars(ap.parse_args())
+SRCPATH = "flanges"
+OUTPATH = "temp"
 
-srcpath = os.path.join(srcpath, f"{args['image']}.jpg")
+BLUR_KERNEL_SIZE = 5
 
-class Init:
-    def __init__(self, init_: bool):
-        self.init = init_
-_init = Init(False)
+# Calculate Canny parameters based on median intensity of filtered image
+# https://stackoverflow.com/questions/41893029/opencv-canny-edge-detection-not-working-properly
+def canny_calc(img, s = 0.33):
+    v = np.median(img)
+    if v > 191: # light images
+        th1 = int(max(0, (1.0 - 2 * s) * (255 - v)))
+        th2 = int(max(85, (1.0 + 2 * s) * (255 - v)))
+    elif v > 127:
+        th1 = int(max(0, (1.0 - s) * (255 - v)))
+        th2 = int(max(255, (1.0 + s) * (255 - v)))
+    elif v < 63: # dark images
+        th1 = int(max(0, (1.0 - 2 * s) * v))
+        th2 = int(max(85, (1.0 + 2 * s) * v))
+    else:
+        th1 = int(max(0, (1.0 - s) * v))
+        th2 = int(max(85, (1.0 + s) * v))
+    return (th1, th2)
+
+def find_contour(img):
+    contours, h = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+    circles = []
+    for c in contours:
+        approx = cv.approxPolyDP(c, 0.02 * cv.arcLength(c, True), False) # Closed contour boolean set to false as many holes are open; (cv.isContourConvex(approx)) and (area > 3) also removed
+        if (True):#(len(approx) > 8)
+            circles.append(c);
+    return circles
+
+def find_hCircle(img):
+    (lower, upper) = canny_calc(img)
+    circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, 20, param1 = upper, param2 = 75, minRadius = 0, maxRadius = 0)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+    return circles
 
 def main():
-    cap = cv.imread(srcpath)
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", type = str, required = True, help = "Image index")
+    args = vars(ap.parse_args())
+    
+    frame = cv.imread(os.path.join(SRCPATH, f"{args['image']}.jpg"))
 
-    if cap is None:
+    if frame is None:
         print("Unable to load picture")
         exit()
 
-    frame = cap
-
     frame = ip.resizeImg(frame, width = 500)
 
-    original = frame
-    frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-##    frame = cv.GaussianBlur(frame, (5, 5), 0) # Review effect
-    frame = cv.medianBlur(frame, 3)
-
-##    frame = cv.bilateralFilter(frame, 5, 175, 175)
-    frame = cv.Canny(frame, 75, 100)
-
-    contours, h = cv.findContours(frame, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-    circles = []
-    for c in contours:
-        approx = cv.approxPolyDP(c, 0.05 * cv.arcLength(c, True), True)
-##        if ((len(approx) > 8) and (cv.isContourConvex(approx)) and (area > 3)):
-##            circles.append(c);
-        circles.append(c);
-            
-##            cv.imshow(original)
-##            cv.waitKey(0)
-    cv.drawContours(original, circles, -1, (0, 0, 255), -1)
-            
-##    # Morphological open to remove black noise blobs
-##    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3,3))
-##    frame = cv.morphologyEx(frame, cv.MORPH_OPEN, kernel, iterations = 2)
-##
-##    # Find contours
-##    cnts = cv.findContours(frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-##    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-
-##    for c in cnts:
-##        pm = cv.arcLength(c, True)
-##        approx = cv.approxPolyDP(c, 0.04 * pm, True)
-##        area = cv.contourArea(c)
-##        if len(approx) > 5 and area > 1000 and area < 500000:
-##            ((x, y), r) = cv.minEnclosingCircle(c)
-##            cv.circle(frame, (int(x), int(y)), int(r), (0, 0, 255), 2)
-
-    frame = cv.cvtColor(frame, cv.COLOR_GRAY2BGR)
+    original = frame.copy()
     
-    frame = cv.hconcat((frame, original))
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame = cv.medianBlur(frame, BLUR_KERNEL_SIZE)
+    
+    (lower, upper) = canny_calc(frame)
+    frame = cv.Canny(frame, lower, upper)
+
+    contours = find_contour(frame)
+    img_cnt = cv.cvtColor(frame, cv.COLOR_GRAY2BGR)
+    img_cnt = cv.drawContours(img_cnt, contours, -1, (0, 0, 255), -1)
+
+    hCircles = find_hCircle(frame)
+    img_hough = cv.cvtColor(frame, cv.COLOR_GRAY2BGR)
+
+    for i in hCircles[0, :]: # Unpacks 1xNx3 array into Nx3 array
+        center = (i[0], i[1])
+        cv.circle(img_hough, center, 1, (0, 0, 255), 3)
+        
+        radius = i[2]
+        cv.circle(img_hough, center, radius, (0, 0, 255), 3)
+    
+    frame = cv.hconcat((original, img_hough, img_cnt))
     cv.imshow("Results", frame)
     
     if cv.waitKey(0) == ord('s'):
         t = time.strftime("%Y%m%d-%H%M%S")
-        cv.imwrite(os.path.join(outpath, f"test_{t}.png"), frame)
+        cv.imwrite(os.path.join(OUTPATH, f"test_{t}.png"), frame)
 
     cv.destroyAllWindows()
-        
 
 if __name__ == "__main__":
     main()
